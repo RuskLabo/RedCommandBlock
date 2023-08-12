7db0a0e76559e84a20b5b91aca640930c753d56d
RuskLabo <83647593+RuskLabo@users.noreply.github.com>
2023-08-13 08:14:03 +0900

    Upload

---
+++ src/main/java/com/ruskserver/redcommandblock/RedCommandBlock.java
@@ -0,0 +1,17 @@
+package com.ruskserver.redcommandblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RedCommandBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
