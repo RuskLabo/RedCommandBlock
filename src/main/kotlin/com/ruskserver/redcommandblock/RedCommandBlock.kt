c6604502c557329be4b5452bf53c23268a136e38
RuskLabo <83647593+RuskLabo@users.noreply.github.com>
2023-11-04 19:13:19 +0900

    Add Config

--- src/main/kotlin/com/ruskserver/redcommandblock/RedCommandBlock.kt
+++ src/main/kotlin/com/ruskserver/redcommandblock/RedCommandBlock.kt
@@ -6,10 +6,12 @@ class RedCommandBlock : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         logger.info("&c[RedCommandBlock]&7 by RuskLabo")
+        saveDefaultConfig()
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
-        logger.info("&c[RedCommandblock]&7 shutdown")
+        logger.info("&c[RedCommandBlock]&7 shutdown")
+        saveDefaultConfig()
     }
 }
